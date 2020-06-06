df30669f2bf48e035e31c9c64500abc5d081e529
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-06 20:32:52 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,11 +20,11 @@ class Form extends React.Component {
   }
   
   handleSubmit = event => {
-  event.preventDefault()
-  
-  let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
-  this.sendFormDataSomewhere(formData)
-}
+    event.preventDefault()
+    let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
 
 render() {
   return (
